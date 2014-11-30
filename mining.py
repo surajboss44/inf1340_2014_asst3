e2c692d7f039299c06f038cb9e39cd09078d6df8
Sam Novak <sam@server.local>
2014-11-30 14:29:32 -0500

    Formatting changes remove variables

--- mining.py
+++ mining.py
@@ -20,7 +20,7 @@ stock_year_list = []
 global final_list
 final_list = []
 
-def read_stock_data(stock_file_name):
+def read_stock_data(stock, stock_file_name):
     """
 
     :param stock_file_name:
@@ -34,20 +34,11 @@ def read_stock_data(stock_file_name):
             global stock_records
             stock_records = json.loads(stock_file_content)
             calculate_stock_price(stock_records)
-            #return(calculate_stock_price(stock_records))
-            #return(six_worst_months())
-            #return(six_best_months())
-            # print(stock_records)
-            #print(stock_records[0])
         except ValueError:
             return "No values in the JSON file"
     except FileNotFoundError:
         return "JSON file not found"
-        # try:
-        # stock_records = json.loads(stock_file_content)
-        #  function1(stock_records, '2008', '08')
-        #except ValueError:
-        #    return "No values in the JSON file"
+
     except FileNotFoundError:
         return "JSON file not found"
 
@@ -59,11 +50,7 @@ def calculate_stock_price(input_stock_records):
     :return:
     """
     stock_year_list_with_duplicates = []
-    # closing_stock_value = []
-    #monthly_sales = 0
-    #monthly_volume = 0
-    #monthly_sales_data = ()
-    #for item in range(len(input_stock_records)):
+
     for each_stock_record in input_stock_records:
         stock_year_month = each_stock_record["Date"][0:7]
         stock_year_list_with_duplicates.append(stock_year_month)
@@ -74,7 +61,6 @@ def calculate_stock_price(input_stock_records):
         average = calculate_average_for_a_month(input_stock_records, date_item)
         monthly_stock_average = (date_item, round(average,2))  # Tuple for storing monthly average stock price
         final_list.append(monthly_stock_average)  # List for storing average stock price of all months
-    #print("final_list "+str(final_list))
     return final_list
 
 
@@ -100,7 +86,6 @@ def calculate_average_for_a_month(input_stock_records, month_val):
 
 def six_worst_months():
     six_worst_months_data = sorted(final_list, key=itemgetter(1))
-    # six_worst_months_data_sorted = sorted(six_worst_months_data, key=itemgetter(1))
     return six_worst_months_data[0:6]
 
 
@@ -110,9 +95,7 @@ def six_best_months():
 
     :return:
     """
-    six_best_months_data = []
-    six_best_months_data_sorted = []
-    # six_best_months_data = sorted(final_list, key=itemgetter(1))
+
     six_best_months_data_sorted = sorted(final_list, reverse=True, key=itemgetter(1))
     return six_best_months_data_sorted[0:6]
 
@@ -125,6 +108,3 @@ def read_json_from_file(file_name):
     return json.loads(file_contents)
 """
 
-read_stock_data("data\GOOG.json")
-print(six_best_months())
-print(six_worst_months())
