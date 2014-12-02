7b6178ffcf08f58e0cd65793a222163d38ed4ac9
Sam Novak <sam@server.local>
2014-12-01 19:01:00 -0500

    Added new test case and related json file

--- mining.py
+++ mining.py
@@ -30,6 +30,8 @@ def read_stock_data(stock, stock_file_name):
     global final_list
     clear_globals()
 
+    if stock == "":
+        raise ValueError
     try:
         with open(stock_file_name, "r") as file_reader:
             global stock_file_content
@@ -39,10 +41,10 @@ def read_stock_data(stock, stock_file_name):
             stock_records = json.loads(stock_file_content)
             calculate_stock_price(stock_records)
         except ValueError:
-            return ValueError("No values in the JSON file")
+            raise ValueError("JSON file is invalid")
 
     except FileNotFoundError:
-        return FileNotFoundError("JSON file not found")
+        raise FileNotFoundError("JSON file not found")
 
 
 def clear_globals():
@@ -68,13 +70,18 @@ def calculate_stock_price(input_stock_records):
     for each_stock_record in input_stock_records:
         stock_year_month = each_stock_record["Date"][0:7]
         stock_year_list_with_duplicates.append(stock_year_month)
+
     for temp_item in stock_year_list_with_duplicates:
         if temp_item not in stock_year_list:  # Removing duplicate occurrence of YYYY-MM
             stock_year_list.append(temp_item)
+
     for date_item in stock_year_list:
         average = calculate_average_for_a_month(input_stock_records, date_item)
         monthly_stock_average = (date_item, round(average, 2))  # Tuple for storing monthly average stock price
         final_list.append(monthly_stock_average)  # List for storing average stock price of all months
+
+    if len(final_list) < 6:  # file must contain at least 6 months of data  to list of best and worst 6 months
+        raise ValueError("Less than six months data in file")
     return final_list
 
 
@@ -105,6 +112,8 @@ def six_worst_months():
     global final_list
     global six_worst_months_data
     six_worst_months_data = sorted(final_list, key=itemgetter(1))
+    # if len(six_worst_months_data_sorted) < 6:
+    #     raise ValueError("Less than six months data in file")
     return six_worst_months_data[0:6]
 
 
@@ -115,6 +124,8 @@ def six_best_months():
     global final_list
     global six_best_months_data_sorted
     six_best_months_data_sorted = sorted(final_list, reverse=True, key=itemgetter(1))
+    # if len(six_best_months_data_sorted) < 6:
+    #     raise ValueError("Less than six months data in file")
     return six_best_months_data_sorted[0:6]
 
 
@@ -125,6 +136,6 @@ def read_json_from_file(file_name):
 
     return json.loads(file_contents)
 """
-#print(read_stock_data("GOOG", ""))
+#print(read_stock_data("GOOG", "data/empty.json"))
 #
 # print(six_worst_months())
--- test_mining.py
+++ test_mining.py
@@ -27,18 +27,30 @@ def test_tse():
     assert six_worst_months() == [('2009-03', 1.74), ('2008-11', 2.08), ('2008-12', 2.25), ('2009-02', 2.41),
                                   ('2009-04', 2.75), ('2009-01', 3.14)]
 
+def test_no_file_found():
+    """
+    Tests the result if file name is not specified.
+    """
+    with pytest.raises(FileNotFoundError):
+        read_stock_data("GOOG", "")
 
-# def test_no_file_found():
-#     """
-#     Tests the result if file name is not specified.
-#     """
-#     with pytest.raises(FileNotFoundError):
-#         read_stock_data("GOOG", "")
 
-#
-# def test_no_stock_name():
-#     """
-#     Tests the result if stock name is not specified.
-#     """
-#     with pytest.raises(ValueError):
-#         read_stock_data("GOOG", "data/empty.json")
+def test_empty_json():
+    """
+    Tests the result if JSON file is empty or invalid.
+    """
+    with pytest.raises(ValueError):
+        read_stock_data("GOOG", "data/empty.json")
+
+
+def test_no_stock():
+    """
+    Tests the result if stock name is not specified.
+    """
+    with pytest.raises(ValueError):
+        read_stock_data("", "data/GOOG.json")
+
+
+def test_too_few_months():
+    with pytest.raises(ValueError):
+        read_stock_data("GOOG","data/toofew.json")
